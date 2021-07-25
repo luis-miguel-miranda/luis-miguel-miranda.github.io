efd979ed235798b9c46b5cadddf38efb5bf3f58b
Luis Miguel Miranda <miguel@l3m.dev>
2021-07-26 01:49:50 +0200

    Remove _site

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "A Study in Scarlet. Also an experiment in writing and Jekyll",
-        "excerpt":"Maintaining a blog is something that never attracted me or piqued my interest in the slightest. Quite possibly still doesn’t. Technology and experimenting always did though, since I disassembled my first electronic appliance. That experiment brought down the fuse in my house and nearly gave my mother a heart attack,...","categories": ["start","github","jekyll"],
-        "tags": [],
-        "url": "http://localhost:4000/start/github/jekyll/a-study-in-scarlet/",
-        "teaser": null
-      }]
